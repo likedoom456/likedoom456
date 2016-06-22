a3e46f6d5bc655bdaedc6d67e708098a26fcc74f
likedoom456 <451156713@qq.com>
2016-06-22 20:10:52 +0800

    15

---
+++ src/E14.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+//输入某年某月某日，判断这一天是这一年的第几天？(闰年： 西元年份除以400余数为0的，或者除以4为余数0且除以100不为余数0的，为闰年。)
+public class E14 {
+    public static void main(String[] args) {
+
+    }
+}
