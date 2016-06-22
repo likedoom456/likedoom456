6bd42d640881f4f8a5ebcb4ef66075ebb10f10cf
likedoom456 <451156713@qq.com>
2016-06-22 16:57:34 +0800

    EEEEEEEEEEE

---
+++ src/E10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+public class E10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input N:");
+        float n = scanner.nextFloat();
+        float sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += sum(i);
+        }
+        System.out.println(10 / Math.pow(2, n));
+        System.out.println(sum);
+    }
+
+    static float sum(float n) {
+        if (n == 1) {
+            return 10;
+        } else {
+            n = 2 * (float) (10 / (Math.pow(2, n - 1)));
+            return n;
+        }
+    }
+}
