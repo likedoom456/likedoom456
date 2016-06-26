622998aafd3ba5ee92fa518785115199e0559c9f
likedoom456 <451156713@qq.com>
2016-06-26 15:10:25 +0800

    EEEEEEEEEEE

--- src/E33.java
+++ src/E33.java
@@ -3,7 +3,42 @@
  */
 public class E33 {
     public static void main(String[] args) {
+        int n = 10;
+        for (int l = n + 2; l >= 0; l--) {
+            System.out.print(" ");
+        }
+        System.out.println(1);
+        for (int i = 2; i < n + 1; i++) {
+            for (int k = (n + 1 - i); k >= 0; k--) {
+                System.out.print(" ");
+            }
+            System.out.print(1+" ");
+            for (int j = 2; j < i + 1; j++) {
+                System.out.print(" " + line(j, i) +" ");
+            }
+            System.out.println(" "+1);
+        }
+    }
+
+
+    static int line(int x, int y) {
+        if (y == 1 || x == 1) {
+            return 1;
+        } else if (y == 2) {
+
+            if (x == 2) {
+                return 2;
+            } else if (x == 3) {
+                return 1;
+            }
+        } else if (x < y + 1) {
+            int a;
+            a = line(x - 1, y - 1) + line(x, y - 1);
+            return a;
+        }
+        return 1;
 
     }
+
 }
 
