939c7a01c9d3c7c45552c58696fa3ed7c6ea9b2c
likedoom456 <451156713@qq.com>
2016-07-05 20:00:28 +0800

    E28

--- src/exercise/E28.java
+++ src/exercise/E28.java
@@ -12,13 +12,15 @@ public class E28 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int[] a1 = new int[10];
+        System.out.println("输入10个数，谢谢！");
         for (int i=0;i<a1.length;i++) {
             a1[i]=scanner.nextInt();
         }
 
         Arrays.sort(a1);
-        for (int i=0;i<a1.length;i++) {
-            System.out.print(a1[i]);
+        for (int i=0;i<a1.length-1;i++) {
+            System.out.print(a1[i]+",");
         }
+        System.out.println(a1[a1.length-1]);
     }
 }
