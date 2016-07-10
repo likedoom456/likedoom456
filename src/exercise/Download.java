025735e1e22c98408e32438d611c4f1ac4523c06
likedoom456 <451156713@qq.com>
2016-07-10 18:46:16 +0800

    Download

--- src/exercise/Download.java
+++ src/exercise/Download.java
@@ -1,8 +1,6 @@
 package exercise;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -10,20 +8,39 @@ import java.net.URL;
  * Created by Administrator on 2016/7/10.
  */
 public class Download {
-    private static final String link="http://www.vladstudio.com/zh/wallpapers/?skip=0";
+    private static final String link = "http://www.vladstudio.com/zh/wallpapers/?skip=0";
 
     public static void main(String[] args) throws MalformedURLException {
-        URL url=new URL(link);
-        try(BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(url.openStream()))) {
+        int counter = 0;
+        URL url = new URL(link);
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.contains("img")) {
-                   String imgURL=line.substring(line.indexOf("http"),line.indexOf("\" alt"));
-                    System.out.println(imgURL);
+                    String imgURL = line.substring(line.indexOf("http"), line.indexOf("\" alt"));
+
+                    counter++;
+                    save(imgURL,counter);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    static void save(String url, int a) throws MalformedURLException {
+        URL link = new URL(url);
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(link.openStream());
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("d:/" + a+".jpg"))) {
+            int b;
+            while ((b = bufferedInputStream.read()) != -1) {
+                bufferedOutputStream.write(b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
+
